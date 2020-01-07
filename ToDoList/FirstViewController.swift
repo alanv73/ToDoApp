17dfb4b16de93dc7dcb3fdd9ae8dcf01648c9488
Alan Van Art <alanv73@gmail.com>
2020-01-07 13:04:52 -0500

    Initial Commit

---
+++ ToDoList/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  ToDoList
+//
+//  Created by Alan Van Art on 1/7/20.
+//  Copyright © 2020 Alan Van Art. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ToDoList/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  ToDoList
+//
+//  Created by Alan Van Art on 1/7/20.
+//  Copyright © 2020 Alan Van Art. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
